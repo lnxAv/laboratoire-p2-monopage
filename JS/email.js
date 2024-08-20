a566d75f3d08dea5c740404f23d9bbec9a442361
Lnx <46533182+lnxAv@users.noreply.github.com>
2024-08-19 21:24:31 -0400

    Add chrome compatibility

--- JS/email.js
+++ JS/email.js
@@ -1,25 +1,26 @@
 const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 function getSavedEmail() {
-  const email = JSON.parse(localStorage.getItem("email"));
+  const email = JSON.parse(window.localStorage.getItem("email"));
+  console.log(window.localStorage);
   return email;
 }
 
 function putEmail(email) {
-  let newEmails = JSON.parse(localStorage.getItem("email"));
+  let newEmails = JSON.parse(window.localStorage.getItem("email"));
   if (!newEmails) {
     newEmails = [email];
-    localStorage.setItem("email", JSON.stringify([email]));
+    window.localStorage.setItem("email", JSON.stringify([email]));
   } else {
     newEmails.push(email);
-    localStorage.setItem("email", JSON.stringify(newEmails));
+    window.localStorage.setItem("email", JSON.stringify(newEmails));
   }
 
   return newEmails;
 }
 
 function getLogInEmail() {
-  const email = JSON.parse(localStorage.getItem("emailLogin"));
+  const email = JSON.parse(window.localStorage.getItem("emailLogin"));
   return email;
 }
 
@@ -27,7 +28,7 @@ function putLogInEmail(email) {
   const savedEmail = getSavedEmail();
   if (!savedEmail) return false;
   if (savedEmail.includes(email)) {
-    localStorage.setItem("emailLogin", JSON.stringify(email));
+    window.localStorage.setItem("emailLogin", JSON.stringify(email));
     return true;
   } else {
     return false;
@@ -44,10 +45,10 @@ function isLogged() {
 }
 
 function getUser() {
-  const user = JSON.parse(localStorage.getItem("user"));
+  const user = JSON.parse(window.localStorage.getItem("user"));
   return user;
 }
 
 function putUser(user) {
-  localStorage.setItem("user", JSON.stringify(user));
+  window.localStorage.setItem("user", JSON.stringify(user));
 }
